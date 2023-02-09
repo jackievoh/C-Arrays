39c11d0639cf4e550d50cb317e34fac7bb91448e
jackievoh <72594447+jackievoh@users.noreply.github.com>
2023-02-09 16:19:03 -0500

    Add files via upload

---
+++ Deliverable 4/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = 25;
+
+int[] fibonacci = new int[n];
+
+fibonacci[0] = 0;
+fibonacci[1] = 1;
+
+for (int i = 2; i < n; i++)
+{
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Fibonacci number (" + i + ") = " + fibonacci[i]);
+}
+
+Console.ReadLine();
